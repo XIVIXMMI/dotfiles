8510e273489a6524f1df527b58d4ff85ee3cccd2
XIVIXMMI <lethaikhoinguyen@gmail.com>
2025-04-19 03:18:11 +0700

    config tmux

--- macos/.config/wezterm/wezterm.lua
+++ macos/.config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("Hack Nerd Font", {
 	weight = "Medium",
 })
-config.font_size = 15
+config.font_size = 14
 
 -- color scheme
 config.color_scheme = "Catppuccin Mocha"
